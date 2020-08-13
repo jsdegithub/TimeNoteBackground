3c9d92e2d5022d05bb72f7d4d0ec5da926a3ddec
jsdegithub <def_myself@qq.com>
2020-08-13 11:40:46 +0800

    数据库连接和表结构设计

--- config/config.js
+++ config/config.js
@@ -1,3 +1,10 @@
-module.exports={
-    environment:'dev'
+module.exports = {
+    environment: 'dev',
+    database: {
+        dbName: 'timenote',
+        host:'localhost',
+        port:3306,
+        user:'root',
+        password:'root'
+    }
 }
