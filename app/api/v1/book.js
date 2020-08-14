33e3653a0193471d8edc20d0b7e00928becedf39
jsdegithub <def_myself@qq.com>
2020-08-15 02:09:12 +0800

    获取书籍点赞情况

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -2,7 +2,9 @@ const Router = require('koa-router');
 
 const { HotBook } = require('@models/hot-book');
 const { Book } = require('@models/book');
-const { PositiveIntegerValidator } = require('@validator');
+const { PositiveIntegerValidator, SearchValidator } = require('@validator');
+const { Auth } = require('../../../middlewares/auth');
+const { Favor } = require('../../models/favor');
 
 const router = new Router({
     prefix: '/v1/book'
@@ -22,5 +24,31 @@ router.get('/:id/detail', async (ctx) => {
     const book = new Book(v.get('path.id'));
     ctx.body = await book.detail();
 })
+
+
+router.get('/search', async (ctx) => {
+    const v = await new SearchValidator().validate(ctx);
+    const result = await Book.searchFromYuShu(v.get('query.q'), v.get('query.start'), v.get('query.count'));
+    ctx.body = result;
+})
+
+
+router.get('/favor/count', new Auth().m, async ctx => {
+    const count = await Book.getMyFavorBookCount(ctx.auth.uid)
+    ctx.body = {
+        count
+    }
+})
+
+
+router.get('/:book_id/favor', new Auth().m, async ctx => {
+    const v = await new PositiveIntegerValidator().validate(ctx, {
+        id: 'book_id'
+    })
+    const favor = await Favor.getBookFavor(ctx.auth.uid, v.get('path.book_id'));
+    ctx.body = favor;
+})
+
+
 
 module.exports = router;
--- app/models/book.js
+++ app/models/book.js
@@ -2,6 +2,7 @@ const { sequelize } = require('../../core/db');
 const { Sequelize, Model } = require('sequelize');
 const axios = require('axios');
 const util = require('util');
+const { Favor } = require('./favor');
 
 
 class Book extends Model {
@@ -13,6 +14,20 @@ class Book extends Model {
         const url = util.format(global.config.yushu.detailUrl, this.id);
         const detail = await axios.get(url);
         return detail.data;
+    }
+    static async searchFromYuShu(q, start, count, summary = 1) {
+        const url = util.format(global.config.yushu.keywordUrl, encodeURI(q), count, start, summary);
+        const result = await axios.get(url);
+        return result.data;
+    }
+    static async getMyFavorBookCount(uid) {
+        const count = await Favor.count({
+            where: {
+                type: 400,
+                uid
+            }
+        })
+        return count;
     }
 }
 
